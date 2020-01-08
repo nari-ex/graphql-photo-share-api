84edb9bd66a83e70ba2f8000d5560db479786074
TAKAMURA Narimichi <nari@nari64.com>
2020-01-09 00:56:13 +0900

    fix indent

--- index.js
+++ index.js
@@ -70,28 +70,28 @@ const resolvers = {
                 ...args.input
             }
             photos.push(newPhoto)
-return newPhoto
+            return newPhoto
         }
     },
-Photo: {
-    url: parent => `http://example.com/img/${parent.id}.jpg`,
+    Photo: {
+        url: parent => `http://example.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
         },
-            taggedUsers: parent => tags
-                .filter(tag => tag.photoID === parent.id)
-                .map(tag => tag.userID)
-                .map(userID => users.find(u => u.githubLogin === userID))
-},
-User: {
-    postedPhotos: parent => {
-        return photos.filter(p => p.githubUser === parent.githubLogin)
+        taggedUsers: parent => tags
+            .filter(tag => tag.photoID === parent.id)
+            .map(tag => tag.userID)
+            .map(userID => users.find(u => u.githubLogin === userID))
     },
+    User: {
+        postedPhotos: parent => {
+            return photos.filter(p => p.githubUser === parent.githubLogin)
+        },
         inPhotos: parent => tags
             .filter(tag => tag.userID === parent.id)
             .map(tag => tag.photoID)
             .map(photoID => photos.find(p => p.id === photoID))
-}
+    }
 }
 
 // def server
