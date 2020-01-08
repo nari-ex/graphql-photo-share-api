35e0597c03124891524a33e8e0a0ad5175b1d984
TAKAMURA Narimichi <nari@nari64.com>
2020-01-09 01:07:22 +0900

    [feat] Add timestamp with postPhoto function

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ const resolvers = {
         postPhoto(parent, args) {
             var newPhoto = {
                 id: _id++,
-                ...args.input
+                ...args.input,
+                created: new Date()
             }
             photos.push(newPhoto)
             return newPhoto
@@ -123,16 +124,23 @@ var photos = [
     {
         "id": "1",
         "name": "Dropping the Heart Chute",
-        "description": "The heart chute is one of my favorite chutes", "category": "ACTION",
-        "githubUser": "gPlake"
+        "description": "The heart chute is one of my favorite chutes",
+        "category": "ACTION",
+        "githubUser": "gPlake",
+        "created": "3-28-1977"
     }, {
         "id": "2",
-        "name": "Enjoying the sunshine", "category": "SELFIE", "githubUser": "sSchmidt"
+        "name": "Enjoying the sunshine",
+        "category": "SELFIE",
+        "githubUser": "sSchmidt",
+        "created": "1-2-1985"
     }, {
         "id": "3",
         "name": "Gunbarrel 25",
-        "description": "25 laps on gunbarrel today", "category": "LANDSCAPE",
-        "githubUser": "sSchmidt"
+        "description": "25 laps on gunbarrel today",
+        "category": "LANDSCAPE",
+        "githubUser": "sSchmidt",
+        "created": "2018-04-15T19:09:57.308Z"
     }
 ]
 
